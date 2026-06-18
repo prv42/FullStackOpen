c3ac40a20b9d1f30fc4595f01c256caa1ee2bdd6
Pierre <pv@pierrevu.fr>
2026-06-18 18:10:22 +0200

    feat: add exercise 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -33,8 +33,12 @@ const generateId = () => {
   return String(maxId + 1)
 }
 
-app.use(morgan("tiny"))
+morgan.token("body", (req) => {
+  return req.method === "POST" ? JSON.stringify(req.body) : ""
+})
+
 app.use(express.json())
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
 
 app.get("/api/persons", (request, response) => {
   response.json(persons)
