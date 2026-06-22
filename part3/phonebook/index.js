7e20334168d4a23a840bf8cdaef6d3608788662d
Pierre <pv@pierrevu.fr>
2026-06-22 12:04:22 +0200

    feat: add exercise 3.15

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -70,9 +70,10 @@ app.get("/api/persons/:id", (request, response) => {
 })
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
-  response.status(204).end()
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
 })
 
 app.get("/info", (request, response) => {
