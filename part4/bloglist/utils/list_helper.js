fe13adcdfc2476aff2320f80e26c3dff4760bdc0
Pierre <pv@pierrevu.fr>
2026-06-23 15:40:44 +0200

    feat: add exercise 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,12 @@ const totalLikes = (blogs) => {
   return blogs.reduce((acc, blog) => acc + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.length === 0 ? null : blogs.reduce((favorite, blog) => blog.likes > favorite.likes ? blog : favorite)
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
