f30a29d178d7983f388077233380db1cb2d7d44f
Pierre <pv@pierrevu.fr>
2026-06-23 16:00:53 +0200

    feat: add exercise 4.6

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -10,8 +10,30 @@ const favoriteBlog = (blogs) => {
   return blogs.length === 0 ? null : blogs.reduce((favorite, blog) => blog.likes > favorite.likes ? blog : favorite)
 }
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null
+
+  const counts = blogs.reduce((acc, blog) => {
+    acc[blog.author] ? acc[blog.author]++ : acc[blog.author] = 1
+    return acc
+  }, {})
+
+  let topAuthor = null
+  let maxBlogs = 0
+
+  for (const author in counts) {
+    if (counts[author] > maxBlogs) {
+      topAuthor = author
+      maxBlogs = counts[author]
+    }
+  }
+
+  return { author: topAuthor, blogs: maxBlogs }
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
