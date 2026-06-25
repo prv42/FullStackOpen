eb82a83cdfa10645e24ec7e75a2daca75b0baa1b
Pierre <pv@pierrevu.fr>
2026-06-25 16:54:19 +0200

    feat: add exercise 4.12

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
+const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 
 const app = express()
@@ -10,5 +11,7 @@ mongoose.connect(config.MONGODB_URI, { family: 4 })
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.errorHandler)
 
 module.exports = app
--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -63,6 +63,30 @@ test('if likes is missing, it defaults to 0', async () => {
   assert.strictEqual(response.body.likes, 0)
 })
 
+test('blog without title is not added', async () => {
+  const newBlog = { author: 'X', url: 'https://example.com/x', likes: 1 }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+})
+
+test('blog without url is not added', async () => {
+  const newBlog = { title: 'X', author: 'X', likes: 1 }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
