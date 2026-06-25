f034a2d9712663a7710f4c1f977ca69ae9fa7250
Pierre <pv@pierrevu.fr>
2026-06-25 15:45:36 +0200

    feat: add exercise 4.8

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,34 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+  { title: '1', author: 'A', url: 'https://example.com/a', likes: 1 },
+  { title: '2', author: 'B', url: 'https://example.com/b', likes: 2 },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(initialBlogs)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('correct amount of blogs is returned', async () => {
+  const response = await api.get('/api/blogs')
+  assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
