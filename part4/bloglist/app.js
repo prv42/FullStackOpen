00a7a7b9960e202e0a21fdef6bfe269bd4fde151
Pierre <pv@pierrevu.fr>
2026-06-27 11:59:09 +0200

    feat: add exercise 4.15

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 const app = express()
 
@@ -11,6 +12,7 @@ mongoose.connect(config.MONGODB_URI, { family: 4 })
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.errorHandler)
 
